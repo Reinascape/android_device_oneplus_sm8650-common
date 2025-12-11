2bd474dfd590c0f2d291bf09781079d17064591f
Michael Bestas <mkbestas@lineageos.org>
2025-12-11 13:30:36 +0200

    sm8650-common: Patch some blobs to depend on libtinyxml2_1.so
    libtinyxml2 10.1.0 update which landed in BP4A contains ABI
    incompatible changes which cause runtime crashes.
    Since the lib lives in system, we can't easily blob it,
    however we can use qualcomm's libtinyxml2_1.so that already exists
    in vendor and seems to work as expected.
    
    Change-Id: Icc67da5f754b0bb30dce6192b1e520fdf1349ac5

--- extract-files.py
+++ extract-files.py
@@ -54,7 +54,27 @@ lib_fixups: lib_fixups_user_type = {
 
 blob_fixups: blob_fixups_user_type = {
     'odm/bin/hw/vendor.oplus.hardware.biometrics.fingerprint@2.1-service_uff': blob_fixup()
-        .add_needed('libshims_aidl_fingerprint_v3.oplus.so'),
+        .add_needed('libshims_aidl_fingerprint_v3.oplus.so')
+        .replace_needed('libtinyxml2.so', 'libtinyxml2_1.so'),
+    (
+        'odm/bin/touchDaemon',
+        'odm/bin/hw/vendor-oplus-hardware-touch-V2-service',
+        'vendor/bin/poweropt-service',
+        'vendor/bin/qvrdatauploader',
+        'vendor/lib64/hw/libaudioeffecthal.qti.so',
+        'vendor/lib64/libaodoptfeature.so',
+        'vendor/lib64/libapengine.so',
+        'vendor/lib64/libcamerapoweroptfeature.so',
+        'vendor/lib64/liblearningmodule.so',
+        'vendor/lib64/libgamepoweroptfeature.so',
+        'vendor/lib64/libpowercallback.so',
+        'vendor/lib64/libpowercore.so',
+        'vendor/lib64/liboffscreenpoweroptfeature.so',
+        'vendor/lib64/libpsmoptfeature.so',
+        'vendor/lib64/libstandbyfeature.so',
+        'vendor/lib64/libvideooptfeature.so',
+    ): blob_fixup()
+        .replace_needed('libtinyxml2.so', 'libtinyxml2_1.so'),
     'product/etc/sysconfig/com.android.hotwordenrollment.common.util.xml': blob_fixup()
         .regex_replace('/my_product', '/product'),
     'system_ext/bin/horae': blob_fixup()
